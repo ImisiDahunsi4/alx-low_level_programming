325bd5c52882ef4d31d6aa8eb03ab2e4879bb7c8
Imisioluwa <imisidahunsi12@gmail.com>
2023-07-21 16:48:19 -0700

    compilation completed

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: The product of the multiplication
+ */
+int mul(int a, int b)
+{
+    return (a * b);
+}
+
---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Calls the print_numbers function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    print_numbers();
+    return (0);
+}
+
