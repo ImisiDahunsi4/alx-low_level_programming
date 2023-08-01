5a32f6674598367041dba74ac902757f9e84664b
Imisioluwa <imisidahunsi12@gmail.com>
2023-08-01 15:59:17 -0700

    yalavakira

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+char *_strstr(char *haystack, char *needle)
+{
+    while (*haystack != '\0')
+    {
+        char *l = haystack;
+        char *p = needle;
+
+        while (*l == *p && *p != '\0')
+        {
+            l++;
+            p++;
+        }
+
+        if (*p == '\0')
+            return (haystack);
+
+        haystack++;
+    }
+    return (0);
+}
+
---
+++ 0x07-pointers_arrays_strings/7-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char board[8][8] = {
+        {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+        {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+    };
+    print_chessboard(board);
+    return (0);
+}
+
