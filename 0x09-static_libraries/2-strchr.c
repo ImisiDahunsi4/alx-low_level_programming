d23db656a7532b33926980059e79fd2dcc2e5dac
Imisioluwa <imisidahunsi12@gmail.com>
2023-08-09 05:30:01 -0700

    lets go

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - Locates a character in a string.
+ * @s: The string to search.
+ * @c: The character to locate.
+ *
+ * Return: Pointer to the first occurrence of the character, or NULL if not found.
+ */
+char *_strchr(char *s, char c)
+{
+    int i = 0;
+
+    for (; s[i] != '\0'; i++)
+    {
+        if (s[i] == c)
+            return (&s[i]);
+    }
+
+    return (NULL);
+}
---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copy a string.
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copy.
+ *
+ * Return: Pointer to the destination string.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+    int j = 0;
+
+    while (j < n && src[j] != '\0')
+    {
+        dest[j] = src[j];
+        j++;
+    }
+
+    while (j < n)
+    {
+        dest[j] = '\0';
+        j++;
+    }
+
+    return (dest);
+}
