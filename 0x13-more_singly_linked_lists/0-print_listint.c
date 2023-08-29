c65aa6179334ee4077327f300928766e54c7f577
Imisioluwa <imisidahunsi12@gmail.com>
2023-08-29 15:27:55 -0700

    updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,19 +2,11 @@
 
 
 /**
-
- * print_listint - prints all the elements of a linked list
-
- * @h: linked list of type listint_t to print
-
- *
-
- * Return: number of nodes
-
- */
-
+* print_listint - prints all the elements of a linked list
+* @h: linked list of type listint_t to print
+* Return: number of nodes
+*/
 size_t print_listint(const listint_t *h)
-
 {
 	size_t num = 0;
 
@@ -26,7 +18,7 @@ size_t print_listint(const listint_t *h)
 		num++;
 		h = h->next;
 
-        }
+	}
 	return (num);
 
 }
