ed12b4db4d5733402bd4c144af9b7461c65b1747
Imisioluwa <imisidahunsi12@gmail.com>
2024-01-15 13:54:20 -0800

    test

---
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies memory area.
+ * @dest: Memory where data is stored.
+ * @src: Memory where data is copied from.
+ * @n: Number of bytes to copy.
+ *
+ * Return: Pointer to the destination memory area.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int r = 0;
+
+	for (; r < n; r++)
+	{
+		dest[r] = src[r];
+	}
+
+	return (dest);
+}
